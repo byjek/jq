76fe7bf726af69a579696787bd9e899dc79ceb71
Maciek <maciej.byjos@gmail.com>
2016-12-28 09:20:47 +0100

    2

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+	console.log('DOM za³adowany - mo¿na siê bawiæ');
+});
+
+var span = $("span");
+span.each(function(index, element) {
+});
+
+$("span:odd").css('color','red');
+
+var paragraphs = $('p');
+paragraphs.each(function(index, element) {
+
+var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
+$(element).append(button) 
+
+});
+
+$("button").click(function(){
+	alert($(this).attr("data-tmp"));
+});
